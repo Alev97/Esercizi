a294a1b066291be71daa49ab1eaff10bcf93a68d
Alev97 <alessandro97.vitale@gmail.com>
2024-04-18 14:00:03 +0200

    esrcizio 6 fatto

--- Lezione2/lezione2.py
+++ Lezione2/lezione2.py
@@ -51,3 +51,10 @@ print(filename.removesuffix('txt'))
 names: list = ['Mario', 'Luca', 'Franco']
 for name in names:
     print(name)
+
+# 3-2. Greetings: Start with the list you used in Exercise 3-1, but instead of just printing each person’s name, print a message to them.
+# The text of each message should be the same, but each message should be personalized with the person’s name.
+
+names: list = ['Mario', 'Luca', 'Franco']
+for name in names:
+    print(f'Ciao {name}, ti va di prendere un caffè?')
