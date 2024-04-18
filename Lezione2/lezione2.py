9715ae7128d106b4c0d73caab369755e771a6133
Alev97 <alessandro97.vitale@gmail.com>
2024-04-18 13:30:39 +0200

    esercizio 4 fatto

--- Lezione2/lezione2.py
+++ Lezione2/lezione2.py
@@ -38,4 +38,9 @@ cit: str = 'una volta disse,"Elementare Watson"'
 
 print(f'{name}, {cit}')
 
+# 2-8. File Extensions: Python has a removesuffix() method that works exactly like removeprefix(). 
+# Assign the value 'python_notes.txt' to a variable called filename. 
+# Then use the removesuffix() method to display the filename without the file extension, like some file browsers do.
 
+filename: str = 'python_notes.txt'
+print(filename.removesuffix('txt'))
