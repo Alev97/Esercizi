90b2aacb6df842181468e0e09cd0a0b3013731f0
Alev97 <alessandro97.vitale@gmail.com>
2024-04-18 12:51:42 +0200

    esercizio 2 fatto

--- Lezione2/lezione2.py
+++ Lezione2/lezione2.py
@@ -13,3 +13,17 @@ message: str = f'Ciao {name}, ti va di imparare un po di python insieme?'
 
 print(message)
 
+# 2-3. Personal Message: Use a variable to represent a person’s name, and print a message to that person.
+# Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+
+# questa è una variabile che contiene il nome di una persona
+name: str = 'Mario'
+
+# questa variabile minuscolo
+name_lower: str = name.lower()
+
+#questa variabile maiuscolo
+name_upper: str = name.upper()
+
+# print(f"{name}, {name.upper()}, {name.lower()}")
+print(f'{name}, {name_upper}, {name_lower}')
