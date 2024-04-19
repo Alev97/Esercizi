0188d8675bbbbbea52c4d1febc546dacd096a550
Alev97 <alessandro97.vitale@gmail.com>
2024-04-19 13:01:59 +0200

    esercizio 2 fatto

--- Lezione4/lezione4.py
+++ Lezione4/lezione4.py
@@ -9,3 +9,15 @@ x = 15
 y = 7
 out = subtract(x, y)
 print(out)
+
+# Esercizio 2
+
+def check_value(x: int):
+    if x > 5:
+        print(f'{x} è maggiore di 5')
+    elif x < 5:
+        print(f'{x} è minore di 5')
+    else:
+        print(f'{x} è uguale a 5')
+
+check_value(22)
