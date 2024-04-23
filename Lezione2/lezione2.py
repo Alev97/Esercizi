4500a38cadc569430c3273865e7c59046462ffa1
Alev97 <alessandro97.vitale@gmail.com>
2024-04-23 19:23:32 +0200

    esrcizio 3-6 fatto

--- Lezione2/lezione2.py
+++ Lezione2/lezione2.py
@@ -88,7 +88,22 @@ guest_list.insert(3, 'Franceca')
 for name in guest_list:
     print(f'Hi {name}, do you want come...')
 
-# 
+# 3-6. More Guests: You just found a bigger dinner table, so now more space is available. Think of three more guests to invite to dinner.
+# • Start with your program from Exercise 3-4 or 3-5. Add a print() call to the end of your program, informing people that you found a bigger table.
+# • Use insert() to add one new guest to the beginning of your list.
+# • Use insert() to add one new guest to the middle of your list.
+# • Use append() to add one new guest to the end of your list.
+# • Print a new set of invitation messages, one for each person in your list.
+
+print(f'Hi {guest_list}, I found a bigger table')
+guest_list.insert(0, 'Alex')
+guest_list.insert(2, 'Maria')
+guest_list.append('Pietro')
+for name in guest_list:
+    print(f'Hi {name}, we are a lot, come to dinner')
+
+
+
 
 
 
