5eee31c4fc321549e36d3c31d885762f977045cf
Alev97 <alessandro97.vitale@gmail.com>
2024-04-30 19:34:10 +0200

    fare esercizio 6-12

--- Lezione2/lezione2.py
+++ Lezione2/lezione2.py
@@ -310,3 +310,12 @@ for city, info in cities.items():
     print(f"Country: {info['country']}")
     print(f"Population: {info['population']}")
     print(f"Fact: {info['fact']}")
+
+"""
+6-12. Extensions: We’re now working with examples that are complex enough 
+that they can be extended in any number of ways. Use one of the example programs from this chapter, 
+and extend it by adding new keys and values, changing the context of the program,
+or improving the formatting of the output.
+"""
+
+# FARE IN SEGUITO
