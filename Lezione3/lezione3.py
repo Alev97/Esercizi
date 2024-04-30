b6884f2bafdac5326e2fb5fbc1647dc08ceca329
Alev97 <alessandro97.vitale@gmail.com>
2024-04-30 22:00:35 +0200

    esercizio 4-7 fatto

--- Lezione3/lezione3.py
+++ Lezione3/lezione3.py
@@ -78,3 +78,12 @@ print(sumnums)
 oddnums = list(range(1,21,4))
 for nums in oddnums:
     print(nums)
+
+"""
+4-7. Threes: Make a list of the multiples of 3, from 3 to 30.
+ Use a for loop to print the numbers in your list.
+"""
+
+multiples_of_3 = list(range(3,31,3))
+for nums in multiples_of_3:
+    print(nums)
