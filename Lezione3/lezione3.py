05407e71ccf1cdf93b84025ffd0818a7b5b3b94b
Alev97 <alessandro97.vitale@gmail.com>
2024-04-30 22:45:24 +0200

    esercizio 4-11 fatto

--- Lezione3/lezione3.py
+++ Lezione3/lezione3.py
@@ -106,3 +106,35 @@ for nums in cubenums:
 
 cube = [nums ** 3 for nums in range(1,11)]
 print(cube)
+
+"""
+4-10. Slices: Using one of the programs you wrote in this chapter, 
+      add several lines to the end of the program that do the following:
+• Print the message The first three items in the list are:.
+  Then use a slice to print the first three items from that program’s list.
+• Print the message Three items from the middle of the list are:. 
+  Then use a slice to print three items from the middle of the list.
+• Print the message The last three items in the list are:. 
+  Then use a slice to print the last three items in the list.
+"""
+
+# NON HO CAPITO CHE DEVO FARE
+
+"""
+4-11. My Pizzas, Your Pizzas: Start with your program from Exercise 4-1. 
+Make a copy of the list of pizzas, and call it friend_pizzas. Then, do the following:
+• Add a new pizza to the original list.
+• Add a different pizza to the list friend_pizzas.
+• Prove that you have two separate lists. Print the message My favorite pizzas are:,
+ and then use a for loop to print the first list. Print the message My friend’s favorite pizzas are:, 
+ and then use a for loop to print the second list. Make sure each new pizza is stored in the appropriate list.
+"""
+
+my_favpizza = ['Margherita','Patate','Capricciosa']
+friend_pizza = my_favpizza[:]
+my_favpizza.append('Crostino')
+friend_pizza.append('Marinara')
+for pizza in my_favpizza:
+    print(f'My favourite pizza are: {pizza}')
+for pizza in friend_pizza:
+    print(f"My friend's pizza are: {pizza}")
