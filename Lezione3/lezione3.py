1144f1a612deb3eee3caaf36913b2e9b0f3500d6
Alev97 <alessandro97.vitale@gmail.com>
2024-04-30 21:54:06 +0200

    esercizio 4-6 fatto

--- Lezione3/lezione3.py
+++ Lezione3/lezione3.py
@@ -69,3 +69,12 @@ print(minnums)
 print(maxnums)
 print(sumnums)
 
+"""
+
+4-6. Odd Numbers: Use the third argument of the range() function to make a list
+ of the odd numbers from 1 to 20. Use a for loop to print each number.
+"""
+
+oddnums = list(range(1,21,2))
+for nums in oddnums:
+    print(nums)
