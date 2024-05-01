9150754710b1bc7c8667e86ef22d32105008f4f5
Alev97 <alessandro97.vitale@gmail.com>
2024-05-01 23:51:49 +0200

    esercizio 8-9 fatto

--- Lezione4/lezione4.py
+++ Lezione4/lezione4.py
@@ -131,6 +131,32 @@ print(city_country('Roma', 'Italy'))
 print(city_country('Mosca', 'Russia'))
 print(city_country('Rabat', 'Marocco'))
 
+'''
+8-7. Album: Write a function called make_album() that builds a dictionary describing a music album. 
+The function should take in an artist name and an album title, 
+and it should return a dictionary containing these two pieces of information. 
+Use the function to make three dictionaries representing different albums. 
+Print each return value to show that the  dictionaries are storing the album information correctly. 
+Use None to add an optional parameter to make_album() that allows you to store the number of songs on an album. 
+If the calling line includes a value for the number of songs, add that value to the album’s dictionary. 
+Make at least one new function call that includes the number of songs on an album.
+'''
+
+# NON HO CAPITO CHE DEVO FARE
 
 
+'''
+# PER L'ESERCIZIO 8-8 DEVO AVER FATTO L'8-7 CHE NON HO CAPITO
 
+'''
+
+'''
+8-9. Messages: Make a list containing a series of short text messages. 
+Pass the list to a function called show_messages(), which prints each text message.
+'''
+
+messages = ['Ciao come stai?','Ho molta fame','Mamma mia che noia','Stasera usciamo?']
+def show_messages(messages):
+    for message in messages:
+        print(message)
+show_messages(messages)
