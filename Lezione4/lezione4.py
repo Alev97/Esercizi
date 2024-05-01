076f4212ae29aa30a638cf3cec0276cc22eefecd
Alev97 <alessandro97.vitale@gmail.com>
2024-05-02 00:01:23 +0200

    esercizio 8-10 fatto

--- Lezione4/lezione4.py
+++ Lezione4/lezione4.py
@@ -160,3 +160,27 @@ def show_messages(messages):
     for message in messages:
         print(message)
 show_messages(messages)
+
+'''
+8-10. Sending Messages: Start with a copy of your program from Exercise 8-9. 
+Write a function called send_messages() that prints each text message and 
+moves each message to a new list called sent_messages as it’s printed. 
+After calling the function, print both of your lists to make sure the messages were moved correctly.
+
+'''
+
+def show_messages(messages):
+    for message in messages:
+        print(message)
+
+def send_messages(messages):
+    sent_messages = []
+    while messages:
+        old_message = messages.pop(0) 
+        print(old_message)
+        sent_messages.append(old_message)
+    return sent_messages
+messages = ['Ciao come stai?','Ho molta fame','Mamma mia che noia','Stasera usciamo?']
+sent_messages = send_messages(messages[:])
+print("\nOld Messages List:", messages)
+print("Sent Messages List:", sent_messages)
