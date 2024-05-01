aa60f7b853996ad7e8e6f574d0c32618a10bc9de
Alev97 <alessandro97.vitale@gmail.com>
2024-05-01 22:47:16 +0200

    esercizio 8-3 fatto

--- Lezione4/lezione4.py
+++ Lezione4/lezione4.py
@@ -80,3 +80,18 @@ Call the function, making sure to include a book title as an argument in the fun
 def favorite_book(title):
     print(f'One of my favorite books is {title}')
 favorite_book('Supersantos')
+
+'''
+8-3. T-Shirt: Write a function called make_shirt() that accepts a size and the text 
+of a message that should be printed on the shirt. 
+The function should print a sentence summarizing the size of the shirt and the message printed on it. 
+Call the function once using positional arguments to make a shirt. 
+Call the function a second time using keyword arguments.
+'''
+
+def make_shirt(size,message):
+    print(f"The size is {size} and the message is: {message}.")
+make_shirt('M',"Don't worry, be happy")
+make_shirt(size = 'XXL', message = 'Carpe diem')
+
+
