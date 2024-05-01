2c22f9fcb0ca3a05f2556b8cd48bdc09c8b681ec
Alev97 <alessandro97.vitale@gmail.com>
2024-05-01 23:32:50 +0200

    esercizio 8-6 fatto

--- Lezione4/lezione4.py
+++ Lezione4/lezione4.py
@@ -119,7 +119,17 @@ describe_city('Madrid','Spain')
 describe_city('Sevilla','Spain')
 describe_city('Oslo','Norway')
 
+'''
+8-6. City Names: Write a function called city_country() that takes in the name of a city and its country. 
+The function should return a string formatted like this: "Santiago, Chile". 
+Call your function with at least three city-country pairs, and print the values that are returned.
+'''
 
+def city_country(city, country):
+    return f'{city}, {country}'
+print(city_country('Roma', 'Italy'))
+print(city_country('Mosca', 'Russia'))
+print(city_country('Rabat', 'Marocco'))
 
 
 
