5b7a3e143d775d1198bc6bae8ce71d7d2c49c9ea
Alev97 <alessandro97.vitale@gmail.com>
2024-05-01 13:01:09 +0200

    esercizio 5-6 fatto

--- Lezione3/lezione3.py
+++ Lezione3/lezione3.py
@@ -263,3 +263,28 @@ elif alien_color == 'yellow':
     print("You earned 10 points for shooting the yellow alien!")
 else:
     print("You earned 15 points for shooting the red alien!")
+
+"""
+5-6. Stages of Life: Write an if-elif-else chain that determines a person’s stage of life.
+ Set a value for the variable age, and then:
+• If the person is less than 2 years old, print a message that the person is a baby.
+• If the person is at least 2 years old but less than 4, print a message that the person is a toddler.
+• If the person is at least 4 years old but less than 13, print a message that the person is a kid.
+• If the person is at least 13 years old but less than 20, print a message that the person is a teenager.
+• If the person is at least 20 years old but less than 65, print a message that the person is an adult.
+• If the person is age 65 or older, print a message that the person is an elder.
+"""
+
+age = 74
+if age < 2:
+    print('It is a baby.')
+elif age < 4:
+    print('It is a toddler.')
+elif age < 13:
+    print('It is a kid.')
+elif age < 20:
+    print('It is a teenager.')
+elif age < 65:
+    print('It is an adult.')
+else:
+    print('It is an elder.')
