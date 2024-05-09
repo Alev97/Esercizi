ade2f69ec2845d7199fa132f415aa07fce356349
Alev97 <alessandro97.vitale@gmail.com>
2024-05-09 19:07:27 +0200

    lezione6 esercizi ripasso

--- Lezione6/lezione6.py
+++ Lezione6/lezione6.py
@@ -59,7 +59,123 @@ class Restaurant:
 
 r1 = Restaurant(name='La vecchia Roma', cuisine_type='Romana')
 print(r1.number_served)
-r1.set_number_served(2)
+#r1.set_number_served(2)
 print(r1.number_served)
 r1.increment_number()
 print(r1.number_served)
+
+
+# Esercizio di ripasso
+
+
+from typing import Tuple
+
+a: tuple = (1, 2)
+
+class Persona:
+    
+    def __init__(self, 
+                 name: str, 
+                 cognome: str, 
+                 data_di_nascita: str,
+                 genere: str,
+                 codice_fiscale: str) -> None:
+        
+        self.nome: str = name
+        self.cognome: str = cognome
+        self.data_di_nascita: str = data_di_nascita
+        self.genere: str = genere
+        self.codice_fiscale: str = codice_fiscale
+        
+    def calcola_eta(self)->int:
+        
+        return 10
+    
+    def __eq__(self, value: object) -> bool:
+
+        return value.codice_fiscale == self.codice_fiscale
+    
+    __eq__(persona_1, persona_2)
+    persona_1 == Persona_2
+    
+    
+    persona_1: Persona = Persona(name="Flavio", 
+                             cognome="Giorgi", 
+                             data_di_nascita="24/12/94", 
+                             genere="Maschio")
+
+
+class Dipendente(Persona):
+    
+    def __init__(self, 
+                 name: str, 
+                 cognome: str, 
+                 data_di_nascita: str, 
+                 genere: str,
+                 ore_lavorate: int) -> None:
+        super().__init__(name, cognome, data_di_nascita, genere)
+        self.ore_lavorate: int = ore_lavorate
+
+        
+    def calcola_stipendio(self)->float:
+        
+        return 500.0
+
+    def __str__(self) -> str:
+        return super().__str__()
+    
+    def __eq__(self, value: object) -> bool:
+        return super().__eq__(value)
+
+
+dipendente_1: Dipendente = Dipendente(name="Flavio", 
+                             cognome="Giorgi", 
+                             data_di_nascita="24/12/94", 
+                             genere="Maschio",
+                             ore_lavorate=500)
+
+
+
+class Professore(Dipendente):
+    
+    def __init__(self, 
+                 name: str, 
+                 cognome: str, 
+                 data_di_nascita: str, 
+                 genere: str, 
+                 ore_lavorate: int,
+                 materia_insegnata: str,
+                 ore_di_lezione: int) -> None:
+        super().__init__(name, cognome, data_di_nascita, genere, ore_lavorate)
+        
+        self.materia_insegnata: str = materia_insegnata
+        self.ore_di_lezione: int = ore_di_lezione
+        
+    def __str__(self) -> str:
+        return f'Professor {self.nome} {self.cognome}
+
+    def __eq__(self, value: object) -> bool:
+        return super().__eq__(value)
+    
+
+print(persona_1.calcola_eta())
+
+print(dipendente_1.ore_lavorate)
+print(dipendente_1.nome)
+print(dipendente_1.calcola_eta())
+print(dipendente_1.calcola_stipendio())
+
+
+
+professore_1: Professore = Professore(name="Flavio", 
+                             cognome="Giorgi", 
+                             data_di_nascita="24/12/94", 
+                             genere="Maschio",
+                             ore_lavorate=500,
+                             materia_insegnata="Python",
+                             ore_di_lezione=1000)
+
+print(professore_1.ore_di_lezione)
+
+print(str())
+
